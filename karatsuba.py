59fb57b46c07237dcdaf9ca89de2b604b0bd4e4d
Eden Barlow <10960976@uvu.edu>
2024-08-28 18:00:06 -0600

    original karatsuba with ints and divmod

--- karatsuba.py
+++ karatsuba.py
@@ -11,23 +11,9 @@ def karatsuba(x, y):
     m2 = max(len(str(x)), len(str(y))) // 2
 
     # Split the digit sequences in the middle 
-    X = str(x)
-    Y = str(y)
-    xl = len(X)
-    yl = len(Y)
-
-    if xl < yl:
-        n_zeroes = yl-xl
-        X = "0"*n_zeroes + X
-    elif yl < xl:
-        n_zeroes = xl-yl
-        Y = "0"*n_zeroes + Y
-
-    a, b = int(X[:m2]), int(X[m2:])
-    c, d = int(Y[:m2]), int(Y[m2:])
     # divmod returns the div result (the first 1/2 of digits) and the remainder (the second 1/2)
-    # a, b = divmod(x, 10**m2)
-    # c, d = divmod(y, 10**m2)
+    a, b = divmod(x, 10**m2)
+    c, d = divmod(y, 10**m2)
 
     # 3 recursive calls for Karatsuba
     ones = karatsuba(b, d) 
@@ -38,10 +24,10 @@ def karatsuba(x, y):
     return tens*10**(m2*2) + cross*10**m2 + ones
 
 # Example usage
-x = 31415926535897932384626433832793141592653589793238462643383279314159265358979323846264338327931415926535897932384626433832793141592653589793238462643383279
-y = 2718281828459045235360287471352662497757247093699959574966967627314159265358979323846264338327931415926535897932384626433832793141592653589793238462643383279
+x = 3141592653589793238462643383279502884197169399375105820974944592
+y = 2718281828459045235360287471352662497757247093699959574966967627
 
 result = karatsuba(x, y)
 
-print(f"The result of multiplying {x} and {y} is {result}")
+print(f"The result of multiplying {x} and {y} is \n{result}")
 print(x*y)
