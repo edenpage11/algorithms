c6ce4606d5bac0f6280579afee23090a24653bd0
Eden Barlow <10960976@uvu.edu>
2024-10-14 17:05:48 -0600

    get G and G_rev

---
+++ kosaraju/ks.py
@@ -0,0 +1,18 @@
+## read in files and store edges as dictionary
+def get_edges(file):
+    G = {}
+    G_rev = {}
+    with open(file, "r") as f:
+        for line in f:
+            if line[0] in G.keys():
+                G[line[0]] += [line[2]]
+            else: 
+                G[line[0]] = [line[2]]
+            if line[2] in G_rev.keys():
+                G_rev[line[2]] += [line[0]]
+            else: 
+                G_rev[line[2]] = [line[0]]
+
+    print(G, G_rev)
+
+get_edges("kosaraju\ex8.txt")
