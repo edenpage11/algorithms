dd4b9290741a065ffd75c13d2ab3f9f2e0cabcc4
Eden Barlow <10960976@uvu.edu>
2024-11-19 21:19:37 -0700

    added visit function

--- knapsack/node.py
+++ knapsack/node.py
@@ -17,5 +17,15 @@ class Node():
         self.max_excluded = -1
         self.yes = None
         self.no = None
-        
+
+    def visit(self, stack, items):
+        if self.item.level == len(items) - 1:
+            # leaf node
+            print("reached leaf", self.item)
+            return
+        # create 2 new child nodes of next item type
+        self.yes = Node(items[self.item.level + 1])
+        self.no = Node(items[self.item.level + 1])
+        print("created children", self.yes, self.no)
+
 
