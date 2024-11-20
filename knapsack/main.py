244e154c292c78dd7319e2cf0bdd431d4f75bc05
Drew-boi <122581219+Drew-boi@users.noreply.github.com>
2024-11-19 21:40:32 -0700

    Update main.py

--- knapsack/main.py
+++ knapsack/main.py
@@ -15,6 +15,29 @@ def read_file(filename):
             itemList[i].level = i
     return capacity, depth, itemList
 
+def dfs(rootNode):
+    ...
+
+def optimism(node):
+    '''Calculate the optimistic estimate of the value of a node's subtree.'''
+
+    # The items that have not yet been considered (i.e. the items that are below the current node in the tree)
+    # itemList is a global variable that contains a sorted list of all item objects
+    avalaible_items = itemList[node.level:]
+    capacity = node.capacity
+    current_value = node.cur_value
+
+    for item in avalaible_items:
+        if item.weight <= capacity:
+            capacity -= item.weight
+            current_value += item.value
+        else:
+            # If the item does not fit in the knapsack, add the fraction of the item that does fit
+            current_value += item.value * (capacity / item.weight)
+            break
+
+    return current_value
+
 def main():
     filename = "problem16.7test.txt"
     capacity, depth, itemList = read_file(filename)
