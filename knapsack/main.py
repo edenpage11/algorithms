501c07b1835f43e3522e2750a25de49eb4c054cd
Drew-boi <122581219+Drew-boi@users.noreply.github.com>
2024-11-19 21:23:45 -0700

    Populated list

---
+++ knapsack/main.py
@@ -0,0 +1,21 @@
+from node import Item, Node
+
+def read_file(filename):
+    with open(filename, 'r') as f:
+        firstline = f.readline()
+        capacity, depth = firstline.strip().split(" ")
+        itemList = []
+        for line in f:
+            value, weight = line.strip().split(" ")
+            itemList.append(Item(int(value), int(weight)))
+        itemList = sorted(itemList, key=lambda item: item.value / item.weight, reverse=True)
+        for i in range(len(itemList)):
+            itemList[i].level = i
+    return capacity, depth, itemList
+
+def main():
+    filename = "problem16.7test.txt"
+    capacity, depth, itemList = read_file(filename)
+
+if __name__ == '__main__':
+    main()
